4c934b710f8721b14a94a476741e1e5c36097b70
Robert Schrenk <robert@schrenk.cc>
2018-08-13 21:10:21 +0200

    using moodle url module to create icon-paths

--- map.php
+++ map.php
@@ -84,7 +84,6 @@ echo $OUTPUT->render_from_template(
         'goto' => 'list',
         'gotostr' => get_string('list', 'block_gps'),
         'is_https' => \block_gps::is_https(),
-        'wwwroot' => $CFG->wwwroot,
     )
 );
 
