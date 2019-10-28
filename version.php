ceaf0dc6d961b99c03cb8bc35c4ee5ad9c99cbb7
Robert Schrenk <robert@schrenk.cc>
2019-10-28 19:44:15 +0100

    Increment version number.

--- version.php
+++ version.php
@@ -23,8 +23,8 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version  = 2018091201;
+$plugin->version  = 2019102800;
 $plugin->requires = 2014051200;  // Requires Moodle 2.7.
 $plugin->component = 'block_gps';
-$plugin->release = '1.7 (Build: 2018091201)';
+$plugin->release = '1.7 (Build: 2019102800)';
 $plugin->maturity = MATURITY_STABLE;
