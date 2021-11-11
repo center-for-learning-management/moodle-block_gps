e5514505e2a73a5f3e52140f99f8925a9f798d6b
Robert Schrenk <robert@schrenk.cc>
2021-11-11 16:09:53 +0100

    Assistive functions to improve frontend of condition

--- amd/src/geoassist.js
+++ amd/src/geoassist.js
@@ -18,11 +18,13 @@ define(['jquery', 'core/ajax', 'core/notification', 'core/str', 'core/url', 'blo
                                             //alert('less than 5m');
                                             console.log('User moved less than 5m since last position');
                                         } else {
-                                            console.log('There was an error, show notification');
+                                            console.log('There was an error');
+                                            /*
                                             var resStr = str.get_string(result, 'block_gps');
                                             $.when(resStr).done(function(localizedEditString) {
                                                  notification.alert(localizedEditString, '');
                                             });
+                                            */
                                         }
                                     },
                                     fail: notification.exception,
@@ -36,15 +38,14 @@ define(['jquery', 'core/ajax', 'core/notification', 'core/str', 'core/url', 'blo
             }
         },
         current: function(src) {
+            var GEOASSIST = this;
             if (navigator.geolocation) {
                 M.availability_gps.locatebtn = src;
                 M.availability_gps.locatebtn.value = M.str.availability_gps.loading + '...';
                 navigator.geolocation.getCurrentPosition(
                     function(position){
                         M.availability_gps.locatebtn.value = M.str.availability_gps.current_location;
-                        require(['block_gps/geoassist'], function(helper){
-                            helper.coord(position.coords.latitude, position.coords.longitude);
-                        });
+                        GEOASSIST.coord(position.coords.latitude, position.coords.longitude);
                     }
                 )
             } else {
@@ -52,13 +53,29 @@ define(['jquery', 'core/ajax', 'core/notification', 'core/str', 'core/url', 'blo
             }
         },
 
-        init: function(lat, lon) {
+        init: function(toggle) {
+            var GEOASSIST = this;
+            var lat = $('[name=latitude]').val();
+            var lon = $('[name=longitude]').val();
+            var accuracy = $('[name=accuracy]').val();
+
+            if (typeof(toggle) !== 'undefined') {
+                $('#availability_gps_map').toggleClass('hidden');
+                if (lat == 0 && lon == 0 && navigator.geolocation) {
+                    navigator.geolocation.getCurrentPosition(
+                        function(position){
+                            GEOASSIST.coord(position.coords.latitude, position.coords.longitude);
+                        }
+                    )
+                }
+            }
+
             if (typeof M.availability_gps.marker !== 'undefined') {
                 M.availability_gps.marker.setLatLng(L.latLng(lat, lon));
                 M.availability_gps.map.panTo(L.latLng(lat, lon));
+                M.availability_gps.circle.setLatLng(L.latLng(lat, lon));
+                M.availability_gps.circle.setRadius(accuracy);
             } else {
-                Y.one('#availability_gps_map').removeClass('closed');
-                Y.one('#availability_gps_map_info').removeClass('closed');
                 M.availability_gps.map = L.map( 'availability_gps_map', {
                     center: [lat, lon],
                     zoom: 13
@@ -79,20 +96,33 @@ define(['jquery', 'core/ajax', 'core/notification', 'core/str', 'core/url', 'blo
                     .addTo(M.availability_gps.map)
                     .on('dragend', function(){
                         var coord = M.availability_gps.marker.getLatLng();
-                        require(['block_gps/geoassist'], function(helper){
-                            helper.coord(coord.lat, coord.lng);
-                        });
+                        GEOASSIST.coord(coord.lat, coord.lng);
+                    })
+                    .on('move', function(){
+                        var coord = M.availability_gps.marker.getLatLng();
+                        M.availability_gps.circle.setLatLng(coord);
                     }
                 );
+                M.availability_gps.circle = L.circle([lat, lon], {
+                    color: 'red',
+                    fillColor: '#f03',
+                    fillOpacity: 0.2,
+                    radius: accuracy,
+                }).addTo(M.availability_gps.map);
+            }
+        },
+        initIfShown: function() {
+            console.log('initifshown');
+            if (typeof M.availability_gps.marker !== 'undefined') {
+                console.log('is shown');
+                this.init();
             }
         },
         coord: function(lat, lon) {
-            require(['block_gps/geoassist'], function(helper){
-                helper.init(lat, lon);
-            });
             M.availability_gps.node.one('[name=longitude]').set('value', lon);
             M.availability_gps.node.one('[name=latitude]').set('value', lat);
             M.core_availability.form.update();
+            this.init();
         }
     };
 });
--- version.php
+++ version.php
@@ -23,8 +23,8 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version  = 2021111000;
+$plugin->version  = 2021111100;
 $plugin->requires = 2014051200;  // Requires Moodle 2.7.
 $plugin->component = 'block_gps';
-$plugin->release = '2.0';
+$plugin->release = '2.2';
 $plugin->maturity = MATURITY_STABLE;
