4ba1fa37aa91d05d26d7669df2b0aa61b6ec989e
Robert Schrenk <robert@schrenk.cc>
2021-11-18 13:47:59 +0100

    increment version

--- version.php
+++ version.php
@@ -23,8 +23,8 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version  = 2021111701;
+$plugin->version  = 2021111800;
 $plugin->requires = 2014051200;  // Requires Moodle 2.7.
 $plugin->component = 'block_gps';
-$plugin->release = '2.3';
+$plugin->release = '2.3.1';
 $plugin->maturity = MATURITY_STABLE;
